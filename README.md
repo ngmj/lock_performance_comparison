3fbb9f2065a26ebda703847df2edbf7003c7688e
ngmj <ngmj.happy@gmail.com>
2018-06-11 20:34:29 +0800

    modify some files

--- README.md
+++ README.md
@@ -1,2 +1,16 @@
 # lock_performance_comparison
 pthread锁 读写锁 boost 区域锁 读写所性能对比
+
+
+1、raw_lock.cpp raw_rwlock.cpp 
+为对比互斥锁和读写锁 的锁耗时（加完立即释放，读线程1个，写线程1个）
+结论：互斥锁加解锁的耗时要比读写锁的快（1、raw_lock耗时1.832699s；2、raw_rwlock耗时3.620338s）
+
+2、simple_lock.cpp simple_rwlock.cpp 
+对一段临界区进行操作时，读写锁的性能对比（读线程100个，写线程1个）
+结论：如果临界区是一个简单的代码，比如简单的一个赋值操作，那么互斥锁的性能要高于读写锁（1、simple_lock耗时1.221884s；2、simple_rwlock耗时2.235480s）
+      如果临界区是一个map的查找，虽然只有20个kv，但是读写锁性能也要高于互斥锁（1、simple_lock耗时14.058138s；2、simple_rwlock耗时9.445691s）
+
+3、lock.cpp rwlock.cpp
+对一段临界区进行操作时，读写锁的性能对比（读线程100个，写线程1个）
+结论：由于临界区是个比较耗时的操作，计算两个向量的余弦相似度，故读写锁性能要远高于互斥锁（1、lock耗时2.925601s；2、rwlock耗时0.320296s）
--- rwlock.cpp
+++ rwlock.cpp
@@ -2,68 +2,131 @@
 #include <string.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <sys/time.h>
 
-int i=0;
+#include <vector>
+
+#include "fun.hpp" // Cosine( vec1, vec2 )
+
+using namespace std;
+
+volatile float rst = 0;
 pthread_rwlock_t rwlock;
+vector<float> vec1, vec2;
+int d = 1000*1000;
 
-void *thread_1( void *arg )
+void *thread_r( void *arg )
 {
-    int j;
-    volatile int a;
-    for ( j = 0; j< 10000000; j++ )
+    for ( int i = 0; i < 1; i++ )
     {
         pthread_rwlock_rdlock(&rwlock);
-        a = i;
+
+        rst = Cosine( vec1, vec2 );
+
         pthread_rwlock_unlock(&rwlock);
     }   
     return NULL;
 }
 
-void *thread_2( void *arg )
+void *thread_w( void *arg )
 {
-    int j;
-    volatile int b;
-    for ( j = 0; j < 10000000; j++ )
+    srand( time(NULL) );
+    for ( int i = 0; i < 1; i++ )
     {
         pthread_rwlock_wrlock(&rwlock);
-        b = i;
+
+        vec1.clear(), vec2.clear();
+        vec1.reserve(d), vec2.reserve(d);
+
+        for ( int j = 0; j < d; j++ )
+        {
+            vec1.push_back( rand() % d );
+            vec2.push_back( rand() % d );    
+        }
+
         pthread_rwlock_unlock(&rwlock);
     }   
     return NULL;
 }
 
-int main()
+int main( int argc, char *argv[] )
 {
     int err;
-    pthread_t th1,th2;
+    size_t rdThdNum = 0, wrThdNum = 0;
+    struct timeval endTime, startTime;
+    if ( argc > 2 )
+    {
+        rdThdNum = atoi( argv[1] );
+        wrThdNum = atoi( argv[2] );
+    } else if ( argc > 1 )
+    {
+        rdThdNum = atoi( argv[1] );
+        wrThdNum = 1;
+    } else 
+    {
+        rdThdNum = 1;
+        wrThdNum = 1;
+    }
+    fprintf( stdout, "exec [rdThdNum=%d] [wrThdNum=%d]\n", rdThdNum, wrThdNum );
+
+    srand(time(NULL));
+    for ( int j = 0; j < d; j++ )
+    {
+        vec1.push_back( rand() % d );
+        vec2.push_back( rand() % d );    
+    }
+        
+    pthread_t rdThd[rdThdNum], wrThd[wrThdNum];
     pthread_rwlock_init( &rwlock, NULL );
-    err = pthread_create( &th1, NULL, thread_1, (void *)0 );
-    if ( err != 0 )
+
+    gettimeofday( &startTime, NULL );
+    for ( size_t i = 0; i < rdThdNum; i++  )
     {
-        printf( "create new thread error:%s\n", strerror( err ) );
-        exit( 0 );
+        err = pthread_create( &rdThd[i], NULL, thread_r, (void *)0 );
+        if ( err != 0 )
+        {
+            printf( "create new thread error:%s\n", strerror( err ) );
+            exit( 0 );
+        }
     }   
-    err = pthread_create( &th2, NULL, thread_2, (void *)0 );
-    if ( err != 0 )
+
+    for ( size_t i = 0; i < wrThdNum; i++  )
     {
-        printf( "create new thread error:%s\n", strerror( err ) );
-        exit( 0 );
+        err = pthread_create( &wrThd[i], NULL, thread_w, (void *)0 );
+        if ( err != 0 )
+        {
+            printf( "create new thread error:%s\n", strerror( err ) );
+            exit( 0 );
+        }
     }
 
-    err = pthread_join( th1, NULL );
-    if ( err != 0 )
+    for ( size_t i = 0; i < rdThdNum; i++  )
     {
-        printf( "wait thread done error:%s\n", strerror( err ) );
-        exit( 1 );
-    }
-    err = pthread_join( th2, NULL );
-    if ( err != 0 )
-    {
-        printf( "wait thread done error:%s\n", strerror( err ) );
-        exit( 1 );
+        err = pthread_join( rdThd[i], NULL );
+        if ( err != 0 )
+        {
+            printf( "wait thread done error:%s\n", strerror( err ) );
+            exit( 1 );
+        }
     }
 
-    printf( "i=%d\n", i );
+    for ( size_t i = 0; i < wrThdNum; i++  )
+    {
+        err = pthread_join( wrThd[i], NULL );
+        if ( err != 0 )
+        {
+            printf( "wait thread done error:%s\n", strerror( err ) );
+            exit( 1 );
+        }
+    }
+
+
+    gettimeofday( &endTime, NULL );
+
+    float timeCost = ( endTime.tv_sec - startTime.tv_sec ) + 
+        ( endTime.tv_usec - startTime.tv_usec ) / 1000000.0;
+
+    fprintf( stdout, "rst=%f, time cost: %fs\n", rst, timeCost );
     pthread_rwlock_destroy( &rwlock );
     exit(0);
 }
