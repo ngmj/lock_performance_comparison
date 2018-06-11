632ee162747c3a4b29c66a3db5803053c969c541
ngmj <ngmj.happy@gmail.com>
2018-06-11 20:34:07 +0800

    modify some files

---
+++ raw_lock.cpp
@@ -0,0 +1,110 @@
+#include <pthread.h>
+#include <string.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/time.h>
+
+#include <vector>
+#include <map>
+
+using namespace std;
+
+pthread_mutex_t mutex;
+int rnd = 10000000;
+
+void *thread_r( void *arg )
+{
+    volatile int a;
+    for ( int i = 0; i < rnd; i++ )
+    {    
+        pthread_mutex_lock( &mutex );
+        pthread_mutex_unlock( &mutex );
+    }   
+    
+    return NULL;
+}
+
+void *thread_w( void *arg )
+{
+    volatile int b;
+    for ( int i = 0; i < rnd; i++ )
+    {
+        pthread_mutex_lock( &mutex );
+        pthread_mutex_unlock( &mutex );
+    }   
+    return NULL;
+}
+
+int main( int argc, char *argv[] )
+{
+    int err;
+    size_t rdThdNum = 0, wrThdNum = 0;
+    struct timeval endTime, startTime;
+    if ( argc > 2 )
+    {
+        rdThdNum = atoi( argv[1] );
+        wrThdNum = atoi( argv[2] );
+    } else if ( argc > 1 )
+    {
+        rdThdNum = atoi( argv[1] );
+        wrThdNum = 1;
+    } else 
+    {
+        rdThdNum = 1;
+        wrThdNum = 1;
+    }
+    fprintf( stdout, "exec [rdThdNum=%d] [wrThdNum=%d]\n", rdThdNum, wrThdNum );
+
+    pthread_t rdThd[rdThdNum], wrThd[wrThdNum];
+    pthread_mutex_init( &mutex, NULL );
+
+    gettimeofday( &startTime, NULL );
+    for ( size_t i = 0; i < rdThdNum; i++  )
+    {
+        err = pthread_create( &rdThd[i], NULL, thread_r, (void *)0 );
+        if ( err != 0 )
+        {
+            printf( "create new thread error:%s\n", strerror( err ) );
+            exit( 0 );
+        }
+    }   
+
+    for ( size_t i = 0; i < wrThdNum; i++  )
+    {
+        err = pthread_create( &wrThd[i], NULL, thread_w, (void *)0 );
+        if ( err != 0 )
+        {
+            printf( "create new thread error:%s\n", strerror( err ) );
+            exit( 0 );
+        }
+    }
+
+    for ( size_t i = 0; i < rdThdNum; i++  )
+    {
+        err = pthread_join( rdThd[i], NULL );
+        if ( err != 0 )
+        {
+            printf( "wait thread done error:%s\n", strerror( err ) );
+            exit( 1 );
+        }
+    }
+
+    for ( size_t i = 0; i < wrThdNum; i++  )
+    {
+        err = pthread_join( wrThd[i], NULL );
+        if ( err != 0 )
+        {
+            printf( "wait thread done error:%s\n", strerror( err ) );
+            exit( 1 );
+        }
+    }
+
+    gettimeofday( &endTime, NULL );
+
+    float timeCost = ( endTime.tv_sec - startTime.tv_sec ) + 
+        ( endTime.tv_usec - startTime.tv_usec ) / 1000000.0;
+
+    fprintf( stdout, "time cost: %fs\n", timeCost );
+    pthread_mutex_destroy( &mutex );
+    exit( 0 );
+}
---
+++ simple_lock.cpp
@@ -0,0 +1,122 @@
+#include <pthread.h>
+#include <string.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/time.h>
+
+#include <vector>
+#include <map>
+
+#include "fun.hpp"
+
+using namespace std;
+
+volatile float rst = 0;
+pthread_mutex_t mutex;
+map<int, int> kv;
+int rnd = 100000;
+
+void *thread_r( void *arg )
+{
+    volatile int a;
+    for ( int i = 0; i < rnd; i++ )
+    {    
+        pthread_mutex_lock( &mutex );
+        // 读写锁快于互斥锁（即使只有读锁）
+        rst = mapFind( kv, i );
+        // 互斥锁快于读写锁（即使只有读锁）
+        // a = rst;
+        pthread_mutex_unlock( &mutex );
+    }   
+    
+    return NULL;
+}
+
+void *thread_w( void *arg )
+{
+    volatile int b;
+    for ( int i = 0; i < rnd; i++ )
+    {
+        pthread_mutex_lock( &mutex );
+        mapSet( kv ); 
+        // rst = b;
+        pthread_mutex_unlock( &mutex );
+    }   
+    return NULL;
+}
+
+int main( int argc, char *argv[] )
+{
+    int err;
+    size_t rdThdNum = 0, wrThdNum = 0;
+    struct timeval endTime, startTime;
+    if ( argc > 2 )
+    {
+        rdThdNum = atoi( argv[1] );
+        wrThdNum = atoi( argv[2] );
+    } else if ( argc > 1 )
+    {
+        rdThdNum = atoi( argv[1] );
+        wrThdNum = 1;
+    } else 
+    {
+        rdThdNum = 1;
+        wrThdNum = 1;
+    }
+    fprintf( stdout, "exec [rdThdNum=%d] [wrThdNum=%d]\n", rdThdNum, wrThdNum );
+
+    pthread_t rdThd[rdThdNum], wrThd[wrThdNum];
+    pthread_mutex_init( &mutex, NULL );
+
+    mapSet( kv ); 
+
+    gettimeofday( &startTime, NULL );
+    for ( size_t i = 0; i < rdThdNum; i++  )
+    {
+        err = pthread_create( &rdThd[i], NULL, thread_r, (void *)0 );
+        if ( err != 0 )
+        {
+            printf( "create new thread error:%s\n", strerror( err ) );
+            exit( 0 );
+        }
+    }   
+
+    for ( size_t i = 0; i < wrThdNum; i++  )
+    {
+        err = pthread_create( &wrThd[i], NULL, thread_w, (void *)0 );
+        if ( err != 0 )
+        {
+            printf( "create new thread error:%s\n", strerror( err ) );
+            exit( 0 );
+        }
+    }
+
+    for ( size_t i = 0; i < rdThdNum; i++  )
+    {
+        err = pthread_join( rdThd[i], NULL );
+        if ( err != 0 )
+        {
+            printf( "wait thread done error:%s\n", strerror( err ) );
+            exit( 1 );
+        }
+    }
+
+    for ( size_t i = 0; i < wrThdNum; i++  )
+    {
+        err = pthread_join( wrThd[i], NULL );
+        if ( err != 0 )
+        {
+            printf( "wait thread done error:%s\n", strerror( err ) );
+            exit( 1 );
+        }
+    }
+
+    gettimeofday( &endTime, NULL );
+
+    float timeCost = ( endTime.tv_sec - startTime.tv_sec ) + 
+        ( endTime.tv_usec - startTime.tv_usec ) / 1000000.0;
+
+    fprintf( stdout, "rst=%f, time cost: %fs\n", rst, timeCost );
+    pthread_mutex_destroy( &mutex );
+    exit( 0 );
+}
